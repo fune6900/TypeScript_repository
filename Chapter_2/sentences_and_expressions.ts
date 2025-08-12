943ed2b046cadf80e0bec47a466f3cca6f85c6e0
fune6900 <riku.riku1019@icloud.com>
2025-08-12 21:52:28 +0900

    文と式の基本

---
+++ Chapter_2/sentences_and_expressions.ts
@@ -0,0 +1,14 @@
+// 文と式の基本
+const greeting = "Hello, ";
+const target = "World!";
+console.log(greeting + target);
+
+const greeting1 = "Hello, ";
+const target2 = greeting;
+// "Hell, Hello,"と表示される
+console.log(greeting + target);
+
+const greeting3 = "Hello, ";
+const target3 = greeting3 + "World!";
+console.log(target3)
+
