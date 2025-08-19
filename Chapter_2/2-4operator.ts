09c579b0dc5de8bfcd14c1c374001175207966b9
fune6900 <riku.riku1019@icloud.com>
2025-08-20 07:26:42 +0900

    比較演算子と等価演算子、論理演算子

--- Chapter_2/2-4operator.ts
+++ Chapter_2/2-4operator.ts
@@ -41,3 +41,75 @@ console.log(fullName); // "Alice Smith"
 const fullNameTemplate = `${firstName} ${lastName}`; // テンプレートリテラル
 console.log(fullNameTemplate); // "Alice Smith"
 const fullNameWithAge = `${fullName} is 30 years old.`; // 年齢を含めた文字列
+
+// 比較演算子（大小比較）
+const left1 = -5, right1 = 10;
+console.log(left1 < right1); // true
+
+const left2 = 100n, right2 = 50n;
+console.log(left2 >= right2); // true
+
+const left3 = -10, right3 = 0;
+console.log(left3 > right3); // false
+
+const left4 = 12n, right4 = 8n;
+console.log(left4 <= right4); // false
+
+// 一致判定
+const left5 = 123, right5 = 123;
+console.log(left5 === right5); // true
+console.log(left5 !== right5); // false
+
+// 複雑な例
+import { createInterface } from 'readline';
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('パスワードを入力してください:', (password) => {
+  if (password === 'hogehoge') {
+    console.log('ようこそ！');
+  } else {
+    console.log('誰？');
+  }
+  rl.close();
+});
+
+// ==と===の違い
+const str: any = "123";
+
+console.log(str == 123); // true (型変換が行われる)
+console.log(str === 123); // false (型が異なるため厳密な比較)
+
+// 論理演算子
+const t = true, f = false;
+
+console.log(t && t); // true (AND)
+console.log(t && f); // false (AND)
+console.log(f && f); // false (AND)
+
+console.log(t || t); // true (OR)
+console.log(t || f); // true (OR)
+console.log(f || f); // false (OR)
+
+// 複数条件の判定
+rl.question('数値を入力してください:', (val) => {
+  const num = Number(val);
+  if (num >= 0 && num <= 100) {
+    console.log(`入力された数値は0以上100以下です: ${num}`);
+  } else {
+    console.log('入力された数値は0未満または100を超えています。');
+  }
+  rl.close();
+});
+
+// !演算子
+// if (!Nunber.isNaN(num)) {
+//   console.log('123はNaNではありません。');
+// }
+
+// !!式（真偽値に変換）
+const value = "Hello, World!";
+const isTruthy = !!value; // 真偽値に変換
+console.log(isTruthy); // true (空でない文字列は真)
